9b8d61f96eb411f7044d496913adc2bd49a17102
jiri.grussmann <cz.georgio@gmail.com>
2017-11-20 18:49:22 +0100

    Initial

---
+++ MyApplication/app/src/main/java/com/example/g3org3/horoscopes/HoroscopesList.java
@@ -0,0 +1,13 @@
+package com.example.g3org3.horoscopes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HoroscopesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horoscopes_list);
+    }
+}
